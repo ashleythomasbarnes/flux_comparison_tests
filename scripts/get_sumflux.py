aa5e95e0981126e882d7654e4cad1e23cf9c9f5d
Ashley Barnes <ashleybarnes.astro@gmail.com>
2023-09-12 15:09:00 +0200

    Create get_sumflux.py

---
+++ scripts/get_sumflux.py
@@ -0,0 +1,56 @@
+from glob import glob
+# from casatasks import importfits, imstat
+import numpy as np
+from astropy.table import QTable
+import astropy.units as u
+
+
+which = 'gaussians'
+# which = 'disks'
+
+dir_sim = f'./../data/{which}_input/'
+dir_obs = f'./../data/{which}_observed/'
+
+files_sim = glob(f'{dir_sim}*.fits*')
+files_obs = glob(f'{dir_obs}*.image*')
+
+files_sim.sort()
+files_obs.sort()
+
+sum_sim = ['']*len(files_sim)
+sum_obs = ['']*len(files_sim)
+conf_arr = ['']*len(files_sim)
+wide_arr = ['']*len(files_sim)
+
+for i, file_sim in enumerate(files_sim):
+
+	conf = file_sim.split('/')[-1].split('_')[0]
+	wide = file_sim.split('/')[-1].split('_')[-1].replace('.fits', '')
+	
+	for file_obs in files_obs:
+		if (conf in file_obs) & (wide in file_obs):
+			
+			conf_arr[i] = conf
+			wide_arr[i] = wide
+
+			print(file_sim.split('/')[-1], file_obs.split('/')[-1])
+			
+			if not os.path.exists(file_sim.replace('.fits', '.img')): 
+				importfits(file_sim, file_sim.replace('.fits', '.img'), overwrite=True, beam=['0arcsec', '0arcsec', '0deg'])
+
+			file_sim = file_sim.replace('.fits', '.img')
+
+			stat_sim = imstat(file_sim)
+			stat_obs = imstat(file_obs)
+
+			sum_sim[i] = stat_sim['sum'][0]*u.Jy
+			sum_obs[i] = stat_obs['sum'][0]*u.Jy
+
+table = QTable([conf_arr, wide_arr, sum_sim, sum_obs], 
+		names=('conf', 'wide', 'sum_sim', 'sum_obs'))
+
+table['sum_obs/sum_sim'] = table['sum_obs']/table['sum_sim'] # add ratio
+table.sort(['conf', 'wide']) # sort
+
+table.write(f'table_{which}.fits', overwrite=True)
+table.write(f'table_{which}.csv', overwrite=True)
